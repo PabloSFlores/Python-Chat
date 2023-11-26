b7e9286a63d3192cd4b4ea13ee3b0507d9322073
PabloSFlores <20213tn013@utez.edu.mx>
2023-11-26 00:24:05 -0600

    [ADD] Chat completo

--- src/client.py
+++ src/client.py
@@ -1,2 +1,48 @@
 # Cliente que se conecta al servidor para enviar y recibir información
 import functions as fun
+import socket
+import threading
+
+# Función para enviar mensajes
+def send(socket, address):
+    # Siempre se está escuchando por un nuevo mensaje
+    while True:
+        message = input()
+        # Si decide salir, manda la orden al servidor directamente
+        if message == 'SALIR':
+            socket.sendto(message.encode('utf-8'), address)
+            break
+        # En otro caso, envía el mensaje cifrado
+        ciphered_message = f"{username}: {fun.cesar_cipher(message, send_key)}"
+        socket.sendto(ciphered_message.encode('utf-8'), address)
+
+# Función para recibir mensajes
+def receive(socket, address):
+    # Envia el nombre de usuario para agregarse a la lista de usuarios
+    socket.sendto(username.encode('utf-8'), address)
+    # Siempre está escuchando por nuevos mensajes
+    while True:
+        data = socket.recv(1024).decode('utf-8')
+        print(data)
+        # Si el mensaje tiene contenido, lo descifra con la clave ingresada
+        if len(data.split(':')) > 3:
+            ciphered_message = data.split(':')[3]
+            deciphered_message = fun.cesar_decipher(ciphered_message, receive_key)
+            print(f"Mensaje descifrado: {deciphered_message}")
+
+print('- - CHAT | CESAR - -\n' + 'SALIR) Salir del chat')
+# Obtener nombre de usuario, llave de envío y llave de recepción
+username = input('Ingresa tu nombre de usuario: ')
+send_key = int(input('Ingresa tu llave de cifrado para enviar: '))
+receive_key = int(input('Ingresa tu llave de cifrado para recibir: '))
+# Crear socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Dirección del servidor localhost:8080
+server_address = ('localhost', 8080)
+# Hilos de recepción y envío
+threading_receive = threading.Thread(target=receive, args=(client_socket, server_address), daemon=True)
+threading_send = threading.Thread(target=send, args=(client_socket, server_address))
+threading_receive.start()
+threading_send.start()
+threading_send.join()
+client_socket.close()
--- src/server.py
+++ src/server.py
@@ -1,2 +1,46 @@
 # Servidor para la transmisión de mensajes
-import functions as fun
+import socket
+from datetime import datetime
+
+# Crear socket UDp
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Unir la tupla
+server_socket.bind(("localhost", 8080))
+# Usuarios {(ip: puerto): username}
+users = {}
+
+
+# Función que reenvia el mensaje a todas las direcciones de los usuarios menos al que envió el mensaje
+# Imprime el mensaje y lo reenvia
+def broadcast(message, omit_address):
+    print(message)
+    for address in users:
+        if not omit_address == address:
+            server_socket.sendto(message.encode(), address)
+
+# Servidor siempre escuchando
+while True:
+    # Manejo de excepciones
+    try:
+        # Obtener la información de los usuarios descomponiendo el la respuesta de recvfrom
+        data, address = server_socket.recvfrom(1024)
+
+        # Agregar usuario si es nuevo
+        if not address in users:
+            users[address] = data.decode("utf-8")
+            enter_message = f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] {users[address]} entró al chat."
+            broadcast(enter_message, address)
+            continue
+
+        # Eliminar al usuario del diccionario si decide salir
+        if "SALIR" in data.decode("utf-8"):
+            leave_message = f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] {users[address]} salió del chat."
+            users.pop(address)
+            broadcast(leave_message, address)
+        
+        # Mensaje normal
+        else:
+            message = f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] {data.decode()}"
+            broadcast(message, address)
+    except ConnectionResetError:
+        print("Error")
